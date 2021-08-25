88dd7b919e2b56f8f5703006fb79c6c2e078d2b6
Nithin Joy <56895565+nithin-j@users.noreply.github.com>
2021-08-25 00:08:34 -0500

    added features and fixed issues
    added manifest file to deploy in chrome for testing.
    made few small css customizations to fix an issue with link going out of list border.

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 const inputEl = document.querySelector('#input-el')
 const inputBtn = document.querySelector('#input-btn')
 const ulEl = document.querySelector('#ul-el')
+const alertEL = document.querySelector("#alert-el")
 
 // Will implement myLeads array to grab URL, and the website name later on.  
 //let myLeads = {
@@ -13,13 +14,20 @@ let myLeads = []
 
 
 inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    ulEl.className = 'list-class'
+
+    if(inputEl.value != ''){
+        alertEL.textContent = ''
+        myLeads.push(inputEl.value)
+        ulEl.className = 'list-class'
     
     renderLeads()
 
     //clear out input element
     inputEl.value = ''
+    }
+    else{
+        alertEL.textContent = '*requied field missing'
+    }
 })
 
 function renderLeads(){
