a8e60cc7387b366e71350a121aa167a8b836cbc6
Nithin Joy <56895565+nithin-j@users.noreply.github.com>
2021-08-28 00:38:41 -0500

    made some changes
    Added local storage and option to clear the saved urls

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 const inputEl = document.querySelector('#input-el')
-const inputBtn = document.querySelector('#input-btn')
 const ulEl = document.querySelector('#ul-el')
 const alertEL = document.querySelector("#alert-el")
 
+const inputBtn = document.querySelector('#input-btn')
+const deleteBtn = document.querySelector('#delete-btn')
 // Will implement myLeads array to grab URL, and the website name later on.  
 //let myLeads = {
 //     title: [],
@@ -12,18 +13,23 @@ const alertEL = document.querySelector("#alert-el")
 let myLeads = []
 
 
+if(localStorage.length != 0){
+    renderLeads()
+}
+
 
 inputBtn.addEventListener("click", function() {
 
     if(inputEl.value != ''){
         alertEL.textContent = ''
         myLeads.push(inputEl.value)
-        ulEl.className = 'list-class'
-    
-    renderLeads()
-
-    //clear out input element
-    inputEl.value = ''
+        
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        myLeads.length = 0
+        
+        //clear out input element
+        inputEl.value = ''
+        renderLeads()
     }
     else{
         alertEL.textContent = '*requied field missing'
@@ -36,9 +42,23 @@ function renderLeads(){
     // liNode.appendChild(textNode)
     // ulEl.appendChild(liNode)
 
+    if(localStorage.length){
+        ulEl.className = 'list-class'
+    myLeads = JSON.parse(localStorage.getItem("myLeads"))
     let listItems = ''
     myLeads.forEach(lead => {
         listItems = `${listItems} <li><a target="_blank" href="${lead}">${lead}<a/></li>`
     });
     ulEl.innerHTML = listItems
+    }else{
+        ulEl.innerHTML = ''
+        ulEl.className = 'null-list-class'
+    }
+    
 }
+
+deleteBtn.addEventListener("click", function(){
+    localStorage.clear()
+    renderLeads()
+})
+
