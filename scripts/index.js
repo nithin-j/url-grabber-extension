e499914f14dc066b9e89a903fcc86a2c91224d83
Nithin Joy <56895565+nithin-j@users.noreply.github.com>
2021-08-29 01:10:49 -0500

    added option to get the url of current tab from chrome api

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,11 @@
 const inputEl = document.querySelector('#input-el')
 const ulEl = document.querySelector('#ul-el')
 const alertEL = document.querySelector("#alert-el")
+const tabUrlBtn = document.querySelector("#tab-url-btn")
 
 const inputBtn = document.querySelector('#input-btn')
 const deleteBtn = document.querySelector('#delete-btn')
+
 // Will implement myLeads array to grab URL, and the website name later on.  
 //let myLeads = {
 //     title: [],
@@ -11,13 +13,34 @@ const deleteBtn = document.querySelector('#delete-btn')
 // }
 
 let myLeads = []
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-
+console.log (leadsFromLocalStorage)
 if(localStorage.length != 0){
-    renderLeads()
+    myLeads = leadsFromLocalStorage
+    renderLeads(myLeads)
 }
 
-
+function renderLeads(leads){
+    // const liNode = document.createElement('li')
+    // const textNode = document.createTextNode(inputEl.value)
+    // liNode.appendChild(textNode)
+    // ulEl.appendChild(liNode)
+console.log(typeof leads)
+    if(localStorage.length){
+        ulEl.className = 'list-class'
+        let listItems = ''
+        leads.forEach(lead => {
+            listItems = `${listItems} <li><a target="_blank" href="${lead}">${lead}<a/></li>`
+        });
+        ulEl.innerHTML = listItems
+    }else{
+        ulEl.innerHTML = ''
+        ulEl.className = 'null-list-class'
+    }
+    
+}
+///Event Listensers
 inputBtn.addEventListener("click", function() {
 
     if(inputEl.value != ''){
@@ -25,40 +48,35 @@ inputBtn.addEventListener("click", function() {
         myLeads.push(inputEl.value)
         
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        myLeads.length = 0
         
         //clear out input element
         inputEl.value = ''
-        renderLeads()
+        myLeads = JSON.parse(localStorage.getItem("myLeads"))
+        renderLeads(myLeads)
     }
     else{
         alertEL.textContent = '*requied field missing'
     }
 })
 
-function renderLeads(){
-    // const liNode = document.createElement('li')
-    // const textNode = document.createTextNode(inputEl.value)
-    // liNode.appendChild(textNode)
-    // ulEl.appendChild(liNode)
-
-    if(localStorage.length){
-        ulEl.className = 'list-class'
-    myLeads = JSON.parse(localStorage.getItem("myLeads"))
-    let listItems = ''
-    myLeads.forEach(lead => {
-        listItems = `${listItems} <li><a target="_blank" href="${lead}">${lead}<a/></li>`
-    });
-    ulEl.innerHTML = listItems
-    }else{
-        ulEl.innerHTML = ''
-        ulEl.className = 'null-list-class'
-    }
+tabUrlBtn.addEventListener("click", function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        myLeads = JSON.parse(localStorage.getItem("myLeads"))
+        renderLeads(myLeads)
+    })
+    alertEL.textContent = ''
+            
     
-}
+        
+    //clear out input element
+    inputEl.value = ''
+    
+})
 
 deleteBtn.addEventListener("click", function(){
     localStorage.clear()
-    renderLeads()
+    myLeads = JSON.parse(localStorage.getItem("myLeads"))
+    renderLeads(myLeads)
 })
-
